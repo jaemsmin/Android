a0d93e3771efd8a2a40e4208511ce8a0ec95916f
user <>
2020-01-15 10:46:59 +0900

    invoke 관련 추가 테스트.

--- Invoke/app/src/main/java/com/example/invoke/MainActivity.kt
+++ Invoke/app/src/main/java/com/example/invoke/MainActivity.kt
@@ -10,10 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // Test result
         Log.d("[DEBUG]",MyFunction.invoke("hello"))     // HELLO
         Log.d("[DEBUG]", MyFunction("hello"))           // HELLO
-        Log.d("[DEBUG]",Sample + " Hello~!")           // [Sample의 주소값] Hello~!
+        Log.d("[DEBUG]", "$Sample Hello~!")            // [Sample의 주소값] Hello~!
+        //Log.d("[DEBUG]", Sample("Hello~!") )                    // invoke 사용하지 않아 에러 발생
+        Log.d("[DEBUG]",Sample.plus(" Hello~!"))        //  [Sample의 주소값] Hello~!
+
+        Log.d("[DEBUG]", "$SampleTest Hello~!")         //  주소값 Hello~!
+        Log.d("[DEBUG]",SampleTest.plus(" Hello~!"))     //  plus 실행
 
         /*
         MyFunction.invoke("hello") // HELLO
@@ -31,6 +36,12 @@ class MainActivity : AppCompatActivity() {
     object Sample {
         operator fun plus(str: String):String {
             return this.toString() + str
+        }
+    }
+
+    object SampleTest {
+        operator fun plus(str: String):String {
+            return str.toUpperCase() // 모두 대문자로 바꿔줌
         }
     }
 }
